1c119d60386a00039975d88a99a32fc75f44b97c
Bart Jacobs <developer@bartjacobs.com>
2017-05-04 08:30:30 +0200

    Tip 4: Show visual cues

--- Forms/ViewController.swift
+++ Forms/ViewController.swift
@@ -16,7 +16,39 @@ class ViewController: UIViewController {
     @IBOutlet var lastNameTextField: UITextField!
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
-    
+
+    @IBOutlet var passwordValidationLabel: UILabel!
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup View
+        setupView()
+    }
+
+    // MARK: - View Methods
+
+    fileprivate func setupView() {
+        // Configure Password Validation Label
+        passwordValidationLabel.isHidden = true
+    }
+
+    // MARK: - Helper Methods
+
+    fileprivate func validate(_ textField: UITextField) -> (Bool, String?) {
+        guard let text = textField.text else {
+            return (false, nil)
+        }
+
+        if textField == passwordTextField {
+            return (text.characters.count >= 6, "Your password is too short.")
+        }
+
+        return (text.characters.count > 0, "This field cannot be empty.")
+    }
+
 }
 
 extension ViewController: UITextFieldDelegate {
@@ -28,12 +60,25 @@ extension ViewController: UITextFieldDelegate {
         case lastNameTextField:
             passwordTextField.becomeFirstResponder()
         case passwordTextField:
-            emailTextField.becomeFirstResponder()
+            // Validate Text Field
+            let (valid, message) = validate(textField)
+
+            if valid {
+                emailTextField.becomeFirstResponder()
+            }
+
+            // Update Password Validation Label
+            self.passwordValidationLabel.text = message
+
+            // Show/Hide Password Validation Label
+            UIView.animate(withDuration: 0.25, animations: {
+                self.passwordValidationLabel.isHidden = valid
+            })
         default:
             emailTextField.resignFirstResponder()
         }
         
         return true
     }
-
+    
 }
